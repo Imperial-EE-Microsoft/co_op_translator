9b669442bf1c3f4dde3afad6686f4893c0e9e38c
skytin1004 <skytin1004@gmail.com>
2024-08-04 06:58:49 +0900

    verify test for Azure API

--- src/image_translator/azure_vision_translator.py
+++ src/image_translator/azure_vision_translator.py
@@ -10,6 +10,7 @@ from azure.ai.vision.imageanalysis.models import VisualFeatures
 from azure.core.credentials import AzureKeyCredential
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from msrest.authentication import CognitiveServicesCredentials
+
 from src.config.base import Config
 
 def get_computervision_client():
