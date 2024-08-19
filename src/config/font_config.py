c208d16db561f8e166874c84d9de3b233eecfdfa
skytin1004 <skytin1004@gmail.com>
2024-08-19 23:00:37 +0900

    Modify font_config

--- src/config/font_config.py
+++ src/config/font_config.py
@@ -1,23 +1,29 @@
 import os
+import yaml
 
 class FontConfig:
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../fonts'))
+    FONT_MAPPINGS_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), './font_language_mappings.yml'))
 
-    # Define paths to all the fonts used in the project
-    NOTO_SANS_MEDIUM = os.path.join(BASE_DIR, "NotoSans-Medium.ttf")
-
-    # Define the default font
-    DEFAULT = NOTO_SANS_MEDIUM
-
-    @staticmethod
-    def get_font_path(font_name):
+    def __init__(self):
         """
-        Get the font path by name.
+        Initialize the FontConfig class by loading the font mappings from a YAML file.
+        """
+        with open(self.FONT_MAPPINGS_FILE, 'r', encoding='utf-8') as file:
+            self.font_mappings = yaml.safe_load(file)
+
+    def get_font_path(self, language_code):
+        """
+        Retrieve the font path for a given language code.
 
         Args:
-            font_name (str): The name of the font attribute in the FontConfig class.
+            language_code (str): The language code.
 
         Returns:
-            str: The full path to the font file.
+            str: The full path to the corresponding font file. If the font for the given language is not found,
+                 returns the default font path (NotoSans-Medium.ttf).
         """
-        return getattr(FontConfig, font_name, None)
+        # Get the font name based on the language code. Use the default font if the language is not specified.
+        font_name = self.font_mappings.get(language_code, {}).get('font', 'NotoSans-Medium.ttf')
+        # Return the full path to the font file.
+        return os.path.join(self.BASE_DIR, font_name)
